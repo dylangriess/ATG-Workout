43ea7a946026d34634518f51a014ce368dc0789b
dylangriess <dylangriess@gmail.com>
2022-10-28 15:00:00 -0600

    redo of about grid

--- client/src/pages/About/About.js
+++ client/src/pages/About/About.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid"; // Grid version 1
 import image1 from "../../assets/images/image1.jpeg";
 import image2 from "../../assets/images/image2.jpeg";
@@ -9,28 +10,22 @@ function About() {
   return (
     <div id="about">
       <h1 className="aboutHeader">What is ATG?</h1>
-      <Grid
-        sx={{ margin: "50px", border: "1px solid black" }}
-        container
-        spacing={2}
-        className="aboutContainer"
+      <Box
+        sx={{
+          display: "grid",
+          columnGap: 3,
+          rowGap: 1,
+          gridTemplateColumns: "repeat(2, 1fr)",
+        }}
       >
-        <Grid
-          item
-          xs={12}
-          md={4}
-          style={{ backgroundImage: `url(${image1})` }}
-          className="aboutImage"
-          alt="squat knee exercise"
-          sx={{ border: "1px solid black" }}
-        ></Grid>
-        <Grid
-          item
-          xs={8}
-          textAlign="right"
-          className="aboutText"
-          sx={{ marginBottom: "50px", border: "1px solid black" }}
-        >
+        <div>
+          <img
+            className="aboutImg"
+            style={{ maxWidth: "100%", height: "auto" }}
+            src={image1}
+          />
+        </div>
+        <div>
           <h3>
             Build From The Ground Up. Ankles to knees, knees to hips, hips to
             shoulders… building in this order makes the most bulletproof body
@@ -39,14 +34,8 @@ function About() {
             athleticism and bulletproofing potential if you build out of balance
             from your natural design?
           </h3>
-        </Grid>
-        <Grid
-          item
-          xs={8}
-          textAlign="left"
-          className="aboutText"
-          sx={{ border: "1px solid black" }}
-        >
+        </div>
+        <div>
           <h3>
             The more we build from the top down, the more pain and injuries we
             accumulate and the more gravity-bound we become. The more we build
@@ -54,17 +43,15 @@ function About() {
             injuries, the more athletic we get, and the longer we live with a
             high quality of life.
           </h3>
-        </Grid>
-        <Grid
-          item
-          xs={12}
-          md={4}
-          style={{ backgroundImage: `url(${image2})` }}
-          className="aboutImage"
-          alt="reverse knee flexion"
-          sx={{ marginBottom: "50px", border: "1px solid black" }}
-        ></Grid>
-      </Grid>
+        </div>
+        <div>
+          <img
+            className="aboutImg"
+            style={{ maxWidth: "100%", height: "auto" }}
+            src={image2}
+          />
+        </div>
+      </Box>
       <h1 className="aboutHeader">Get To Know Eric</h1>
       <Grid container spacing={2}>
         <Grid
