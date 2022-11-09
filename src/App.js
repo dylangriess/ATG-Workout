843a6eb739ab136301011b890131adf2b5757566
dylangriess <dylangriess@gmail.com>
2022-11-08 17:45:41 -0700

    changing app.js structure

--- src/App.js
+++ src/App.js
@@ -1,11 +1,6 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/Header/Header.js";
 import Home from "./pages/Home/Home.js";
-import About from "./pages/About/About.js";
-import Landing from "./pages/Landing/Landing.js";
-import Contact from "./pages/Contact/Contact.js";
-import Blog from "./pages/Blog/Blog.js";
 import "./App.css";
 
 function App() {
